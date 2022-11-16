c08d450f6d87bd1dc21355fea376718f42021e41
captain-Arvoy <accofstardust@gmail.com>
2022-11-16 10:05:10 +0530

    practising merge sort

---
+++ Java/DSA/Recursion/divide_n_conquer/Revise_merge.java
@@ -0,0 +1,19 @@
+public class Revise_merge{
+	public static void main (String args[]){
+		int size_of_input = 5;//Size to be inputed by user
+		int input_array[] = new int[n];
+	}
+}
+class Resources{
+		void quick_sort(int arr[], int si, int ei){
+				int pivot = arr[length-1], i = -1, traverser;
+
+				if (arr[traverser] < pivot){
+						i++;
+						int t = arr[i];
+						arr[i] = t;
+						arr[traverser] = t;
+						//Left the problem at swap complete it further from recursive call, HARE KRISNA
+				}
+		}
+}
